a70daef9bc72722c703eaf24c2b3f5405a2e904b
Nitesh Parmar <niteshp2007@gmail.com>
2019-01-07 15:02:29 +0000

    Update Work_Log.py

--- Work_Log.py
+++ Work_Log.py
@@ -1,2 +1,28 @@
+import os
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def work_log():
+    clear()
+    print("WORK LOG")
+    while True:
+        try:
+            choices = ["a", "b", "c"]
+            choice = input("""
+    What would you like to do?
+    a) Add new entry
+    b) Search in existing entries
+    c) Quit program\n""")
+            if choice.lower() not in choices:
+                raise ValueError("Enter the valid choices please. a, b or c")
+        except ValueError as err:
+            clear()
+            print("Unfortunately somthing went wrong sorry.\n{}\nPlease try again".format(err))
+            continue
+        if choice.lower() == "c":
+            break
+
+
 if __name__ == "__main__":
-    print("The Start")
+    work_log()
