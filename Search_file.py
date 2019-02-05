3eed2209061397c382331c04b8cfa143d5cd9d84
Nitesh Parmar <niteshp2007@gmail.com>
2019-02-05 22:26:25 +0000

    started the listing function for displaying results

--- Search_file.py
+++ Search_file.py
@@ -2,11 +2,8 @@ import os
 import datetime
 import csv
 
-def clear(self):
-    os.system('cls' if os.name == 'nt' else 'clear')
 
 class Search_file():
-
     def __init__(self, file):
         self.file = file
         self.work_log = []
@@ -15,6 +12,35 @@ class Search_file():
             self.work_log = list(reader)     
      
     def search_method(self):
+
+        def choice_string(log, x):
+            return ("{}) Title: {}, Date: {}, Time Spent: {}, Extra Notes: {}".format(x, log[0], log[1], log[2], log[3]))
+
+        def list_choices(log):
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("There are {} search results".format(len(log)))
+            x = len(log)
+            y = 0
+            print(log[y])
+            while True:
+                ask_option = input("Would you like to see the next search entry, previous or quit search? Next/Prev/Q ").upper()
+                if ask_option == "NEXT" or ask_option == "N":
+                    y += 1
+                    if (log.index(log[y])-1) == x:
+                        print("This is the last item for the search results.")
+                    else:
+                        print(log[y])
+                elif ask_option == "PREV" or ask_option == "P":
+                    y -= 1
+                    if (y+1) == x:
+                        print("This is the first item for the search results.")    
+                    else:
+                        print(log[y])                    
+                elif ask_option == "Q":    
+                    break     
+                else:
+                    print("Please enter a valid command.")       
+
         while True:
             try:
                 choices = ["a", "b", "c", "d"]
@@ -35,16 +61,19 @@ Please pick an option.\n""")
                 x = 1
                 try:
                     date_search = input("What is the date you are looking for?\nPlease use the DD/MM/YYYY: ")
-                    datetime.datetime.strptime(date_search, '%d/%m/%Y') 
+                    datetime.datetime.strptime(date_search, '%d/%m/%Y')
+                    list_of_choices = [] 
                     for log in self.work_log:
                         if log[3] == "":
                             log[3] = "N/a"
                         if date_search == log[0]:
-                            print("{}) Title: {}, Date: {}, Time Spent: {}, Extra Notes: {}".format(x, log[0], log[1], log[2], log[3]))
-                            x += 1
-
+                            list_of_choices.append(choice_string(log, x))
+                            x += 1      
+                    list_choices(list_of_choices)
                 except ValueError as err:               
                     print("Something went wrong sorry\n{}".format(err))
+                if input("Would you like to search again Y/n? ").upper() != "Y":
+                    break
             elif choice.lower() == "b":
                 ##search by time spent
                 x = 1
@@ -55,7 +84,7 @@ Please pick an option.\n""")
                         if log[3] == "":
                             log[3] = "N/a"
                         if task_time == int(log[2]):
-                            print("{}) Title: {}, Date: {}, Time Spent: {}, Extra Notes: {}".format(x, log[0], log[1], log[2], log[3]))
+                            print(choice_string(log, x))
                             x += 1
                         # elif x == 1:
                         #     print("You entered a invalid number or the task/s with those minutes does not exist.")
@@ -72,7 +101,7 @@ Please pick an option.\n""")
                     if log[3] == "":
                         log[3] = "N/a"
                     if exact_term in log[1].lower() or exact_term in log[3].lower():
-                        print("{}) Title: {}, Date: {}, Time Spent: {}, Extra Notes: {}".format(x, log[0], log[1], log[2], log[3]))
+                        print(choice_string(log, x))
                         x += 1
                     # elif x == 1:
                     #     print("You entered an incorrect term or it does not exist")
@@ -80,5 +109,12 @@ Please pick an option.\n""")
                 if input("Would you like to search again Y/n? ").upper() != "Y":
                     break
             elif choice.lower() == "d":
+                #TODO regex search
                 ##search by regEx
                 break
+
+
+### listing funtion for all choices
+### back and forth method
+### deletion method
+#### Nayans password "741852963"
